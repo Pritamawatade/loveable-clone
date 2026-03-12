864db0f46ad09896d2888f8c1c1d4d833c933200
pritamawatade <pritamawatade5@gmail.com>
2026-03-12 16:15:38 +0530

    setup openai with trpc

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,16 +1,21 @@
 import { z } from 'zod';
 import { baseProcedure, createTRPCRouter } from '../init';
+import { inngest } from '@/inngest/client';
  
 export const appRouter = createTRPCRouter({
-  createAI: baseProcedure
+  invoke: baseProcedure
     .input(
       z.object({
-        text: z.string(),
+        value: z.string(),
       }),
     )
-    .query((opts) => {
+    .mutation(async ({input}) => {
+      await inngest.send({
+        name: "test/hello.world",
+        data: {value: input.value}
+      })
       return {
-        greeting: `hello ${opts.input.text}`,
+        ok: `success`,
       };
     }),
 });
